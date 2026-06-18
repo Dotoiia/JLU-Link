1fd753e6f91787555467d536c49b602036c6247e
Dotoiia <Dotoiia@icloud.com>
2026-06-18 16:52:53 +0800

    Align CI with supported macOS target

--- macoshelper.h
+++ macoshelper.h
@@ -1,7 +1,14 @@
 #ifndef MACOSHELPER_H
 #define MACOSHELPER_H
 
+#include <QtCore/qglobal.h>
+
+#ifdef Q_OS_MACOS
 void SetMacApplicationVisibleInDock(bool visible);
 void InstallMacLiquidGlass(void *nativeView);
+#else
+inline void SetMacApplicationVisibleInDock(bool) {}
+inline void InstallMacLiquidGlass(void *) {}
+#endif
 
 #endif // MACOSHELPER_H
